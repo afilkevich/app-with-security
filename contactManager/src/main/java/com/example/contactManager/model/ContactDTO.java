fb2ff4c924dfb4e807344f4c02109a49467209a7
afilkevich <afilkevich@ya.ru>
2017-09-12 16:33:04 +0300

    create a empty miltimodule project

---
+++ contactManager/src/main/java/com/example/contactManager/model/ContactDTO.java
@@ -0,0 +1,12 @@
+package com.example.contactManager.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by afilkevich on 12.9.17.
+ */@Entity
+@Table(name="contats")
+public class ContactDTO {
+
+}
